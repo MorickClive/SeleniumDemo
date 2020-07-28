393fa9c2f68263410adfab1ffe57c4b407cb8e49
Alex Davies <MorickClive@Gmail.com>
2020-07-28 17:50:20 +0100

    AD: adding an appropriate reference to driver...

--- src/test/java/com/qa/selenium/GooglePageTest.java
+++ src/test/java/com/qa/selenium/GooglePageTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
@@ -15,10 +16,13 @@ public class GooglePageTest {
 
     @Before
     public void setup() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().setSize(new Dimension(1366, 768));
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/selenium/webdrivers/chromedriver_win32/chromedriver.exe");
 
+        ChromeOptions opts = new ChromeOptions();
+        opts.setHeadless(true);
+
+        driver = new ChromeDriver(opts);
+        driver.manage().window().setSize(new Dimension(1920, 1080));
     }
 
     @Test
